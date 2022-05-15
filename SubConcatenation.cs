4bf4bc26cfebc5f9a644c7b8e5909b75a786a4aa
Mike Knoles <MKnoles78@gmail.com>
2022-05-15 13:52:01 -0400

    added code to StringConcatenations.cs

--- SubConcatenation.cs
+++ SubConcatenation.cs
@@ -7,7 +7,14 @@ class StringConcatenation
 {
     public static void Main(string[] args)
     {
+        string string1 = "Happy ";
+        string string2 = "Birthday";
 
-
+        Console.WriteLine( "string1 = \"" + string1 + "\"\n" +
+            "string2 = \"" + string2 +"\"");
+        Console.WriteLine(
+            "\nResult of string.Concat( string1, string2 ) = " +
+            string.Concat(string1, string2));
+        Console.WriteLine("string1 after concatenation = " + string1 );
     } //end Main
 } //end class StringConcatenation
