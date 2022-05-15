48d857bcb016d12163a870900947fdebbc45875a
Mike Knoles <MKnoles78@gmail.com>
2022-05-15 15:47:44 -0400

    syntax corrections

--- CompareStringsPA.cs
+++ CompareStringsPA.cs
@@ -37,7 +37,7 @@ class CompareStringsPA
                 Console.WriteLine("\n Your second password entry {0} is greater than your previous password {1} and doesn't match: ", newPassword, password);
                 Console.WriteLine("Passwords should match.");
                 }
-            else (password.Length > newPassword.Length)
+            else if (password.Length > newPassword.Length)
                 {
                 Console.WriteLine("\n Your second password entry {0} is less than your previous password {1} and doesn't match: ", newPassword, password);
                 Console.WriteLine("Passwords should match.");
