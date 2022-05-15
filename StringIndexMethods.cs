b725288110b9ec905d4f3654a943aaf45d161678
Mike Knoles <MKnoles78@gmail.com>
2022-05-15 13:36:11 -0400

    syntax corrections

--- StringIndexMethods.cs
+++ StringIndexMethods.cs
@@ -13,21 +13,21 @@ class StringIndexMethods
         //test IndexOf to locate a character in a string
         Console.WriteLine($"First 'c' is located at index " +
             letters.IndexOf('c'));
-        Console.WriteLine("First 'a' starting at 1 is located at index" + 
+        Console.WriteLine("First 'a' starting at 1 is located at index " + 
             letters.IndexOf('a', 1));
-        Console.WriteLine("First '$' in the 5 positions strating at 3 " + 
+        Console.WriteLine("First '$' in the 5 positions starting at 3 " + 
             $"is located at index " + letters.IndexOf('$', 3, 5));
 
         //test LastIndexOf to find a character in a string
         Console.WriteLine($"\nLast 'c' is located at index " + 
             letters.LastIndexOf('c'));
         Console.WriteLine("Last 'a' up to position 25 is located at " +
-            "index" + letters.LastIndexOf('a', 25));
+            "index " + letters.LastIndexOf('a', 25));
         Console.WriteLine("Last '$' in the 5 positions ending at 15 " +
             "is located at index " + letters.LastIndexOf('$', 15, 5));
         
         //test IndexOf to locate a substring in a string
-        Console.WriteLine("\nFirst \"def\" starting at 7 is located at " +
+        Console.WriteLine("\nFirst \"def\" is located at " +
             letters.IndexOf("def"));
         Console.WriteLine("First \"def\" starting at 7 is located at " +
             "index " + letters.IndexOf("def", 7));
@@ -39,7 +39,7 @@ class StringIndexMethods
         Console.WriteLine("\nLast \"def\" is located " +
             letters.LastIndexOf("def"));
         Console.WriteLine("Last \"def\" up to position 25 is located " + 
-            "at indext " + letters.LastIndexOf("def", 25));
+            "at index " + letters.LastIndexOf("def", 25));
         Console.WriteLine("Last \"hello\" in the 15 positions " + 
             "ending at 20 is located at index " +
             letters.LastIndexOf("hello", 20, 15));
@@ -50,6 +50,7 @@ class StringIndexMethods
         Console.WriteLine("First 'c', 'a' or '$' starting at 7 is " +
             "located at index " + letters.IndexOfAny(searchLetters, 7));
         Console.WriteLine("First 'c', 'a', or '$' in the 5 positions " +
+            "starting at 7 is located at index " +
             letters.IndexOfAny(searchLetters, 7, 5));
         
         //test LastIndexOfAny to find last occurrence of character
