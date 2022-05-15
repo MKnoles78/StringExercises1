b1f6185be4807e8d682fb89da50846d1f7efba33
Mike Knoles <MKnoles78@gmail.com>
2022-05-15 13:47:33 -0400

    commented out StringIndexMethods to use Main

---
+++ SubConcatenation.cs
@@ -0,0 +1,13 @@
+// Michael Knoles
+// Fig. 16.17: SubConcatenation.cs
+// Demonstrating string class Concat method
+using System;
+
+class StringConcatenation
+{
+    public static void Main(string[] args)
+    {
+
+
+    } //end Main
+} //end class StringConcatenation
