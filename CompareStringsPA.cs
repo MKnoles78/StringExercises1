69bdcba230f042ba676cb83677599b411f91d5f5
Mike Knoles <MKnoles78@gmail.com>
2022-05-15 16:09:23 -0400

    syntax updates

--- CompareStringsPA.cs
+++ CompareStringsPA.cs
@@ -12,7 +12,7 @@ class CompareStringsPA
     public static void Main(string[] args)
     {
         
-        Console.WriteLine("Enter your userName");
+        Console.WriteLine("\nEnter your userName");
         string userName = Console.ReadLine();
         //take in first input value
         Console.WriteLine("\nWelcome {0}! Please enter a password: ", userName);
@@ -23,24 +23,24 @@ class CompareStringsPA
         int status = newPassword.CompareTo(password);
         Console.WriteLine(status);
         // if password when compared to previous password is true, then it returns this message.
-        if (status > 0)
+        if (status == 0)
         {
             Console.WriteLine("\nYour password is {0}: ", password);
             Console.WriteLine("The new password {0} equals your old password {1}. ", newPassword, password);
-            Console.WriteLine("Your password has been stored.");
+            Console.WriteLine("Your password has been stored.\n");
         }
         // else if password when compared to previous is false, then it returns this message.
-        else if (status < 0)
+        else if (status != 0)
         {
             if (password.Length < newPassword.Length)
                 {
                 Console.WriteLine("\nYour second password entry {0} is greater than your previous password {1} and doesn't match: ", newPassword, password);
-                Console.WriteLine("Passwords should match.");
+                Console.WriteLine("Passwords should match.\n");
                 }
-            else
+            else if (password.Length > newPassword.Length)
                 {
                 Console.WriteLine("\nYour second password entry {0} is less than your previous password {1} and doesn't match: ", newPassword, password);
-                Console.WriteLine("Passwords should match.");
+                Console.WriteLine("Passwords should match.\n");
                 }
         }
     } //end Main
