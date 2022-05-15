d2ad62c68b87aa39d5ef21808e701f47773a05f6
Mike Knoles <MKnoles78@gmail.com>
2022-05-15 15:11:23 -0400

    create pseudocode for PA

---
+++ CompareStringsPA.cs
@@ -0,0 +1,15 @@
+//Michael Knoles
+//Unit 1.7 PA: CompareStringsPA.cs
+//Comparing strings 
+using System;
+
+class CompareStringsPA
+{
+    public static void Main(string[] args)
+    {
+        //Take in user input for two values
+        //Store values into strings
+        //Compare values using CompareTo
+        //Output whether first string is less than, equal to or greater than the second value
+    } //end Main
+} //end class CompareStringsPA
